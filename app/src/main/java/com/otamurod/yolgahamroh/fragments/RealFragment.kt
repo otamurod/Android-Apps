70f533d8a7a29ec7c37ac04c95df1a7751923264
otamurod <o.safarov@student.inha.uz>
2022-09-13 16:26:07 +0500

    Android App

--- app/src/main/java/com/otamurod/yolgahamroh/fragments/RealFragment.kt
+++ app/src/main/java/com/otamurod/yolgahamroh/fragments/RealFragment.kt
@@ -33,7 +33,6 @@ class RealFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private val a = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
